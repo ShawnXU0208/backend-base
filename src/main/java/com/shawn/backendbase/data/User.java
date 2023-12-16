799157d3bae3c1122ee386438684bd591f8fc9ee
Shawn Xu <>
2023-12-16 11:33:20 +0800

    feat: add checkstyle config and reformatting code

--- src/main/java/com/shawn/backendbase/data/User.java
+++ src/main/java/com/shawn/backendbase/data/User.java
@@ -1,36 +1,35 @@
 package com.shawn.backendbase.data;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "user", indexes = {
-        @Index(name = "username_index", columnList = "username", unique = true),
-        @Index(name = "email_index", columnList = "email", unique = true)})
+@Table(name = "user", indexes = {@Index(name = "username_index", columnList = "username", unique = true),
+    @Index(name = "email_index", columnList = "email", unique = true)})
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-    @Column(nullable = false, length = 30)
-    private String username;
+  @Column(nullable = false, length = 30)
+  private String username;
 
-    @Column(nullable = false, length = 255)
-    private String email;
+  @Column(nullable = false, length = 255)
+  private String email;
 
-    @Column(nullable = false, length = 30)
-    private String password;
+  @Column(nullable = false, length = 30)
+  private String password;
 
-    @Column(nullable = false, length = 10)
-    private String role;
+  @Column(nullable = false, length = 10)
+  private String role;
 }
